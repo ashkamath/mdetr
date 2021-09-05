b824de8d9d889ba0a20fd389514a0806823af9de
Muhammad Maaz <mmaaz60@gmail.com>
2021-09-05 18:34:19 +0400

    Adds get_class_agnostic_od_annotations_unmerged.py

--- extra_scripts/get_class_agnostic_od_annotations_unmerged.py
+++ extra_scripts/get_class_agnostic_od_annotations_unmerged.py
@@ -2,7 +2,6 @@ import json
 import os
 from pycocotools.coco import COCO
 import time
-import numpy as np
 import argparse
 from PIL import Image
 
@@ -10,7 +9,7 @@ OUTPUT_DIR = "../"
 IMAGES_DIR = ""
 
 
-def filter_annotations(dir_path):
+def get_ca_od_annotations(dir_path):
     updated_file_contents = {"info": [], "licenses": [], "images": [], "annotations": [],
                              "categories": [{'supercategory': 'object', 'id': 1, 'name': 'object'}]}
     images_dict = {}
@@ -32,36 +31,37 @@ def filter_annotations(dir_path):
             image_id = image["id"]
             ann_ids = coco.getAnnIds(imgIds=image_id)
             annotations = coco.loadAnns(ann_ids)
-            annotations_dict[image_name] += annotations
+            annotations_dict[image_name].append(annotations)
         print(f"It took {time.time() - start} seconds to process the file {file}.")
         start = time.time()
     print(f"Assigning ids to images and annotations. Also correcting the width and height values in the annotations.")
     start = time.time()
     # Assign new ids to the images and adjust the annotations accordingly
+    annotation_index = 0
     for i, key in enumerate(images_dict.keys()):
         image = images_dict[key]
-        image["id"] = i
-        annotations = annotations_dict[key]
-        annotations = class_agnostic_nms(annotations)
-        for a in annotations:
-            a["image_id"] = i
-        # Correct the width and height data types
         image_path = f"{IMAGES_DIR}/{image['file_name']}"
         im = Image.open(image_path)
         width, height = im.size
         image["width"] = int(width)
         image["height"] = int(height)
-        updated_file_contents["images"].append(image)
-        updated_file_contents["annotations"] += annotations
+        annotations = annotations_dict[key]
+        for ann in annotations:
+            image["id"] = annotation_index
+            for a in ann:
+                a["image_id"] = annotation_index
+            annotation_index += 1
+            # Correct the width and height data types
+            updated_file_contents["images"].append(image)
+            updated_file_contents["annotations"] += ann
     # Adjust the 'id' of annotations
     for i, ann in enumerate(updated_file_contents["annotations"]):
         ann["id"] = i
     print(f"It took {time.time() - start} seconds for assigning Ids and correcting width and height values.")
     print(f"Correcting category ids.")
     start = time.time()
-    annotations = file_contents["annotations"]
     counter = 0
-    for ann in annotations:
+    for ann in updated_file_contents["annotations"]:
         if ann["category_id"] != 1:
             ann["category_id"] = 1
             counter += 1
@@ -77,6 +77,9 @@ def parse_arguments():
     ap = argparse.ArgumentParser()
     ap.add_argument("-i", "--input_dir_path", required=True,
                     help="Path to the input directory containing json files.")
+    ap.add_argument("-img_dir", "--images_dir_path", required=True,
+                    help="Path to the directory containing corresponding images. Its needed to correct the width and "
+                         "height values in the annotation file.")
     ap.add_argument("-o", "--output_dir_path", required=True,
                     help="Path to the output directory for storing the filtered annotations.")
     args = vars(ap.parse_args())
@@ -87,7 +90,9 @@ def parse_arguments():
 if __name__ == "__main__":
     args = parse_arguments()
     input_dir_path = args["input_dir_path"]
+    images_dir_path = args["images_dir_path"]
     output_dir_path = args["output_dir_path"]
+    IMAGES_DIR = images_dir_path
     OUTPUT_DIR = output_dir_path
     if not os.path.exists(OUTPUT_DIR):
         os.makedirs(OUTPUT_DIR)
